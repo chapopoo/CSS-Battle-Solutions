637099b69ca7c83ae81d98eb7ae6233a4c0b430e
Pooja Chapole <115409936+chapopoo@users.noreply.github.com>
2025-07-07 22:04:22 +0530

    Completed Target - 48

---
+++ Wash Yoir Hands(#Target-48) - index.h
@@ -0,0 +1,22 @@
+<div id="square">
+  <div></div>
+</div>
+<style>
+body{
+  background:#293462;
+}
+#square{
+  width: 200px;
+  height: 100px;
+  background: #FE5F55;
+  border-radius: 20px;
+  box-shadow: 0px 20px 0px #A64942;
+  translate: 92px 82px;
+}
+div > div{
+  width:140px;
+  height:40px;
+  background: #A64942;
+  translate: 30px 30px;
+  border-radius: 20px;
+</style>
